380c87f3bb12088e4220aa9f5f48ed34d1a23822
AMIR <jramir669@gmail.com>
2021-07-08 15:04:15 +0430

    add eslint & pretttier

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'spellcheck'],
+  rules: {
+    'prefer-const': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'spellcheck/spell-checker': [
+      1,
+      {
+        comments: true,
+        strings: false,
+        identifiers: true,
+        lang: 'en_US',
+        skipWords: ['axios', 'antd', 'formik', 'moment'],
+        skipIfMatch: [
+          'http://[^s]*',
+          '^[-\\w]+/[-\\w\\.]+$', // For MIME Types
+          /^[\u0600-\u06FF\s]+$/,
+        ],
+        skipWordIfMatch: [
+          '^foobar.*$', // words that begin with foobar will not be checked
+        ],
+        minLength: 4,
+      },
+    ],
+  },
+}

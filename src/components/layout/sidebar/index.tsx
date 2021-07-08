fb2c3ceb5207b3bc4690c45191b2d7692e7b66bd
AMIR <jramir669@gmail.com>
2021-07-08 19:35:21 +0430

    add side bar skeleton

---
+++ src/components/layout/sidebar/index.tsx
@@ -0,0 +1,10 @@
+// TODO: complete this component
+import React, { ReactElement } from 'react'
+
+interface Props {}
+
+function Sidebar({}: Props): ReactElement {
+  return <div>sidebar</div>
+}
+
+export default Sidebar

e45c0d966cbedbbc1792797de74c937bdee904f2
i-mml <moahamadja114@gmail.com>
2021-07-09 00:07:54 +0430

    create nav menu

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -1,15 +1,42 @@
 // TODO: complete this component
+
 import React, { ReactElement } from 'react'
+
 import Image from 'next/image'
 import logoPic from '@/../public/img/logo.svg'
-
 import s from './styles.module.scss'
 
 function Navbar(): ReactElement {
   return (
     <div className={s.navbar__container}>
-      <h3>navbar</h3>
-      <Image src={logoPic} width={48} height={48} />
+      <div className="row align-items-center">
+        <Image src={logoPic} width={48} height={48} />
+        <div className="col-6 mr-4" style={{ borderRight: '1px solid #ddd' }}>
+          <span
+            className="lnr lnr-map-marker"
+            style={{ fontSize: '16px' }}
+          ></span>
+          <span className={s.city__name}>تهران</span>
+        </div>
+      </div>
+      <div className="row align-items-center">
+        <nav>
+          <ul className="row align-items-center">
+            <li>
+              <span class="lnr lnr-user"></span>
+              <div className="mr-2">دیوار من</div>
+            </li>
+            <li>
+              <span class="lnr lnr-bubble"></span>
+              <div className="mr-2">چت</div>
+            </li>
+            <li>پشتیبانی</li>
+            <li>
+              <button className="btn-primary">ثبت آگهی</button>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,10 @@ export default function Home() {
       </Head>
       <h1>Divar clone</h1>
       <span className="lnr lnr-mustache"></span> lnr-mustache
+      <div className="row">
+        <div className="col-6 bg-danger">col 6 v1</div>
+        <div className="col-6 bg-info">col 6 v2</div>
+      </div>
     </div>
   )
 }

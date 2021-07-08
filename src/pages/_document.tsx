030a199559049b34589a188f3cff34a86e5ee378
AMIR <jramir669@gmail.com>
2021-07-08 22:19:11 +0430

    add linear icons font pack to document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html dir="rtl" lang="fa">
-        <Head />
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css"
+          ></link>
+        </Head>
         <body>
           <Main />
           <NextScript />

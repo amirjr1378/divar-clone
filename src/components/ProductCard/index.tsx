0855665891694c665d28658d6134b44f2443895f
AMIR <jramir669@gmail.com>
2021-07-09 14:50:43 +0430

    split product card type

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,17 +1,18 @@
 // TODO: complete this component
 import React, { ReactElement } from 'react'
 import s from './styles.module.scss'
+import { ProductCardProps } from './types'
 
-interface Props {
-  id: number
-  name?: string
-  price?: number
-  image?: string
-}
-
-function ProductCard(props: Props): ReactElement {
+function ProductCard(props: ProductCardProps): ReactElement {
   const { id, name, price, image } = props
-  return <div className={s.productCard__container}>{name}</div>
+  return (
+    <div className={s.productCard__container}>
+      id: {id}
+      name: {name}
+      price: {price}
+      image: {image}
+    </div>
+  )
 }
 
 export default ProductCard
---
+++ src/components/ProductCard/types.ts
@@ -0,0 +1,6 @@
+export interface ProductCardProps {
+  id: number
+  name?: string
+  price?: number
+  image?: string
+}

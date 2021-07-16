9b5d230fba996458776c746f171b58df44c77db0
AMIR <jramir669@gmail.com>
2021-07-16 12:58:37 +0430

    make size bigger

--- src/components/SelectCity/NormalSelectCity/index.tsx
+++ src/components/SelectCity/NormalSelectCity/index.tsx
@@ -20,7 +20,8 @@ function SelectCity(props: Props): ReactElement {
       <Form.Group className="pl-3">
         <Form.Control
           type="text"
-          placeholder="جستجو شهر"
+          placeholder="جستجوی سریع نام شهر..."
+          size="lg"
           onChange={(e) => {
             searchCity(e.target.value)
           }}
@@ -32,7 +33,7 @@ function SelectCity(props: Props): ReactElement {
         <Row className="mt-3">
           {cities?.length
             ? cities?.map((city) => (
-                <Col key={city.id} sm={6} md={3} lg={2} className="mb-2">
+                <Col key={city.id} sm={6} md={3} lg={2} className="mb-4">
                   <Button
                     variant="outline-primary"
                     className="d-block w-100"

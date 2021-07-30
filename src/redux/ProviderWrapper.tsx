322e49d3c36884c5244a1fc9c572b055d434f12b
AMIR <jramir669@gmail.com>
2021-07-30 15:22:07 +0430

    add redux persist

--- src/redux/ProviderWrapper.tsx
+++ src/redux/ProviderWrapper.tsx
@@ -1,9 +1,15 @@
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
+import { persistor } from './store'
 import { store } from './store'
 
 const ProviderWrapper: React.FC<any> = (props) => (
-  <Provider store={store}>{props.children}</Provider>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      {props.children}
+    </PersistGate>
+  </Provider>
 )
 
 export default ProviderWrapper
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,30 +1,43 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+// redux persist imports
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 // import reducers
 import cityReducer from './features/city/citySlice'
-import {
-  loadStateFromLocalStorage,
-  saveStateToLocalStorage,
-} from './localStorageHelpers'
 
-const persistedState = loadStateFromLocalStorage() || {}
+const rootReducer = combineReducers({
+  city: cityReducer,
+})
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
 
 export const store = configureStore({
   devTools: true,
-  reducer: {
-    city: cityReducer,
-  },
-  preloadedState: persistedState,
+  reducer: persistReducer(persistConfig, rootReducer),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
 
-// for local storage usage
-store.subscribe(() => {
-  saveStateToLocalStorage({
-    city: store.getState().city,
-  })
-})
+export const persistor = persistStore(store)
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch

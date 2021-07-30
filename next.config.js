f0f06732d9611c0df4df195bab1fc00b2d4e671e
AMIR <jramir669@gmail.com>
2021-07-30 17:09:39 +0430

    ignore eslint build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   images: {
     domains: ['s100.divarcdn.com', 's101.divarcdn.com'],

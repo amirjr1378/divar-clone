743363d30a8b234e69b38f975bf19ab56572cb9c
mohammad.javadi <mohamadja114@gmail.com>
2021-08-10 10:22:19 +0430

    handle navbar menu

--- src/components/containers/layout/Navbar/index.tsx
+++ src/components/containers/layout/Navbar/index.tsx
@@ -6,10 +6,11 @@ import React, { ReactElement } from 'react'
 import Button from 'react-bootstrap/Button'
 import Nav from 'react-bootstrap/Nav'
 
-import logoPic from '@/../public/img/logo.svg'
+import logoPic from '@/../public/img/headerI-icon.PNG'
 import { useCity } from '@/redux/hooks'
 
 import s from './styles.module.scss'
+import { Badge} from 'react-bootstrap'
 
 const NavLinkComponent = (props: Record<string, any>) => (
   <Link href={'/'}>
@@ -24,43 +25,61 @@ function Navbar(): ReactElement {
   return (
     <>
       <div className={s.navbar__container}>
-        {/* right content */}
-        <Link href="/">
-          <a>
-            <Image src={logoPic} width={48} height={48} />
-          </a>
-        </Link>
-        <hr />
+        {/*####### right content #######*/}
+        <div className={s.right__content}></div>
 
-        {city?.name && (
-          <div className="d-flex align-items-center">
-            <span className="lnr lnr-map-marker ml-2" />
-            <span className="text-muted">{city?.name}</span>
+        {/*####### center content #######*/}
+        <div className={s.center__content}>
+          <Image src={logoPic} width={100} height={55} />
+        </div>
+
+        {/*####### left content #######*/}
+        <div className={s.left__content}>
+          <div className={s.search_icon_box}>
+            <span className="lnr lnr-magnifier"></span>
+            <span className="mr-1">جستجو</span>
+            {/* <input type="text" placeholder="جستجو کالا ..."/> */}
           </div>
-        )}
-
-        <div className="mr-auto" />
-        {/* left content */}
-        <Nav activeKey="/">
-          <Nav.Item>
-            <Nav.Link href="/me" as={NavLinkComponent}>
-              <span className="lnr lnr-user" />
-              <span>دیوار من</span>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="/chat" as={NavLinkComponent}>
-              <span className="lnr lnr-bubble" />
-              <span>چت</span>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="/support" as={NavLinkComponent}>
-              <span>پشتیبانی</span>
-            </Nav.Link>
-          </Nav.Item>
-        </Nav>
-        <Button type="primry">ثبت آگهی</Button>
+          <Nav activeKey="/">
+            <Nav.Item></Nav.Item>
+            <Nav.Item className="mr-1 ml-1">
+              <Nav.Link
+                href="/chat"
+                as={NavLinkComponent}
+                className={s.navbar__items}
+              >
+                <span className="lnr lnr-bubble" />
+                <span>پشتیبانی</span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item className={s.navbar_login_box}>
+              <Nav.Link
+                href="/login"
+                as={NavLinkComponent}
+                className={s.navbar__items}
+              >
+                <span className="lnr lnr-user" />
+                <span>ورود / ثبت نام</span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="mr-2">
+              <Nav.Link
+                href="/basket"
+                as={NavLinkComponent}
+                className={s.navbar__basket}
+              >
+                <Badge bg="danger" className={s.badge__style}>
+                  7
+                </Badge>
+                <span
+                  className="lnr lnr-cart"
+                  style={{ fontSize: '20px' }}
+                ></span>
+                {/* <span>پشتیبانی</span> */}
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </div>
       </div>
       <div style={{ marginTop: 70 }} />
     </>
